3c5d4806718aeccb74aea4e32e4159527a1d1782
erdey2 <erdeysyoum@gmail.com>
2024-09-18 13:33:01 +0300

    apps registered

--- hotel_reservation/login/admin.py
+++ hotel_reservation/login/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from hotel_reservation.login.models import Customer, RoomManager
+from hotel_reservation.reservation.models import Contact, Room, Reservation
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(RoomManager)
+admin.site.register(Contact)
+admin.site.register(Room)
+admin.site.register(Reservation)
