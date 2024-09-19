832ffc094a824749ac654bb70245b8ae88b6a063
erdey2 <erdeysyoum@gmail.com>
2024-09-19 10:20:31 +0300

    api urls.py updated

--- hotel_reservation/api/urls.py
+++ hotel_reservation/api/urls.py
@@ -1,16 +1,10 @@
-from rest_framework import viewsets
-from hotel_reservation.login.models import Customer
-from hotel_reservation.reservation.models import Reservation, Room
-from . import serializers
-
-class CustomerViewset(viewsets.ModelViewSet):
-    queryset = Customer.objects.all()
-    serializer_class = serializers.UserSerializer
-
-class RoomViewset(viewsets.ModelViewSet):
-    queryset = Room.objects.all()
-    serializer_class = serializers.RoomsSerializer
-
-class ReservationViewset(viewsets.ModelViewSet):
-    queryset = Reservation.objects.all()
-    serializer_class = serializers.BookingSerializer
+from django.urls import path,include
+from rest_framework import routers
+from . import views
+router=routers.DefaultRouter()
+router.register(r'customer', views.CustomerViewset)
+router.register(r'reservation', views.ReservationViewset)
+router.register(r'room', views.RoomViewset)
+urlpatterns = [
+    path('',include(router.urls))
+]
